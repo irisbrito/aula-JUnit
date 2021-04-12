13f545b03cb08bdec7155fc51666101894ef8cc2
Iris <iris.souza@zup.com.br>
2021-04-12 15:00:14 -0300

    Adicionar validação de numero negativo

--- src/main/java/br/com/aula/junit/Agil.java
+++ src/main/java/br/com/aula/junit/Agil.java
@@ -3,6 +3,10 @@ package br.com.aula.junit;
 public class Agil {
 
     public double calcularPert(double horasDificeis, double horasNormais, double horasFaceis){
+        if(horasDificeis < 0 || horasNormais < 0 || horasFaceis < 0){
+            throw new RuntimeException("Horas negativas não são aceitas");
+        }
+
         double resultado = (horasDificeis + horasNormais + horasFaceis) / 6;
         return resultado;
     }
