694129951d7cb8b4725ebc4077d4c63dede9a3e3
Iris <iris.souza@zup.com.br>
2021-04-12 14:32:08 -0300

    Adicionar método de calcular pert

--- src/main/java/br/com/aula/junit/Agil.java
+++ src/main/java/br/com/aula/junit/Agil.java
@@ -1,4 +1,9 @@
 package br.com.aula.junit;
 
 public class Agil {
+
+    public static double calcularPert(double horasDificeis, double horasNormais, double horasFaceis){
+        double resultado = (horasDificeis + horasNormais + horasFaceis) / 6;
+        return resultado;
+    }
 }
