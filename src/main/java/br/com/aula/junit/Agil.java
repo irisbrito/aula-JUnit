08bb5b17b004e9cbc0490a756f2bfce8dcefaa10
Iris <iris.souza@zup.com.br>
2021-04-12 15:20:13 -0300

    Refatorar método de calcular pert

--- src/main/java/br/com/aula/junit/Agil.java
+++ src/main/java/br/com/aula/junit/Agil.java
@@ -3,7 +3,7 @@ package br.com.aula.junit;
 public class Agil {
 
     public double calcularPert(double horasDificeis, double horasNormais, double horasFaceis){
-        if(horasDificeis < 0 || horasNormais < 0 || horasFaceis < 0){
+        if(validarNumerosPositivos(horasDificeis) || validarNumerosPositivos(horasNormais)|| validarNumerosPositivos(horasFaceis)){
             throw new RuntimeException("Horas negativas não são aceitas");
         }
 
